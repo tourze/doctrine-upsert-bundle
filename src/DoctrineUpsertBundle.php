73b6f75a5ab447cb842cd6e1bcd4fcd817520b60
tourze-ci <you@example.com>
2025-04-07 17:40:31 +0000

    feat: 添加doctrine-entity-checker-bundle 依赖并修正类名错误

--- src/DoctrineUpsertBundle.php
+++ src/DoctrineUpsertBundle.php
@@ -3,15 +3,7 @@
 namespace Tourze\DoctrineUpsertBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\DoctrineEntityCheckerBundle\DoctrineEntityCheckerBundle;
 
-class DoctrineUpsertBundle extends Bundle implements BundleDependencyInterface
+class DoctrineUpsertBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            DoctrineEntityCheckerBundle::class => ['all' => true],
-        ];
-    }
 }
