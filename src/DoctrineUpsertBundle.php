12ddd90ef647caad3cd38488369f898c63e6dbb0
github-ci <you@example.com>
2025-06-11 11:25:13 +0800

    update

--- src/DoctrineUpsertBundle.php
+++ src/DoctrineUpsertBundle.php
@@ -2,15 +2,18 @@
 
 namespace Tourze\DoctrineUpsertBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineEntityCheckerBundle\DoctrineEntityCheckerBundle;
 
 class DoctrineUpsertBundle extends Bundle implements BundleDependencyInterface
 {
     public static function getBundleDependencies(): array
     {
         return [
-            \Tourze\DoctrineEntityCheckerBundle\DoctrineEntityCheckerBundle::class => ['all' => true],
+            DoctrineBundle::class => ['all' => true],
+            DoctrineEntityCheckerBundle::class => ['all' => true],
         ];
     }
 }
